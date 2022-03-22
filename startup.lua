a5fdb25438ac11598d096b658643d4925ff3b736
WeasselDW <wesseldewolf2003@gmail.com>
2022-03-22 22:49:53 +0100

    fix

--- startup.lua
+++ startup.lua
@@ -15,19 +15,19 @@ system1 = StorageSystem(256,100,25,10)
 
 
 
-redstoneOutput = class(
+redstoneOutput = class(function()
 
     redstoneOutput.clutch = true
     redstoneOutput.gearshift = false
     redstoneOutput.gantry1 = false
     redstoneOutput.gantry2 = false
     redstoneOutput.sticker = false
-)
+end)
     --[[redstone.setAnalogOutput("right", clutch)
     redstone.setAnalogOutput("left", gearshift)
     redstone.setAnalogOutput("top", g1)
     redstone.setAnalogOutput("back", sticker)
     redstone.setAnalogOutput("front", g2)]]
 
-end
+
 print(system1.width)
